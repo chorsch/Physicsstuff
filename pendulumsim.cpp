6600822f03ea301c351220203dd723c6ec89092e
wchambers96 <will.chambers96@gmail.com>
2016-09-19 17:50:26 -0400

    Add files via upload

---
+++ pendulumsim.cpp
@@ -0,0 +1,47 @@
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+float m=0;
+float l=0;
+float M=0;
+float r=0;
+float R=0;
+float v=0;
+float a=0:
+int void()(
+
+
+double S
+double g = 9.8;
+float theta=0;
+
+/*
+cout << "Please enter the mass of your pendulum rod" << endl;
+cin >> m:
+cout << "please enter the mass of your pendulum weight"<< endl;
+cin >> M ;
+cout << "please enter the length of your pendulum" << endl;
+cin >> l;
+*/
+cout >> "how far did you pull it back? (enter a value of theta between +- 180 degrees)";
+cin >> theta;
+//double E = m*g*sin(theta)*l;
+double I = .25*m*R*R + (1/3)*m*l*l + .5*M*r*r + M*((l+r)*(l+d));
+  double dt=.001;
+  double t
+  v = 0;
+  l = .1;
+  r = .05;
+  m = .15;
+while(t; t < 3000; t=t+dt)(
+  float w = v/(l+r);
+  float Tnet = m*g*l*sin(theta)- ((m*v*v/(l+R))+m*g*sin(theta))*.3*.005;
+  a = Tnet/I
+  theta = theta + wt + .5*a*t*t
+cout << theta << endl;
+)
+
+)
